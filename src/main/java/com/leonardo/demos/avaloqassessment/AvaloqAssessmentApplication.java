163e0e0f40437fb756ad37ccbfee0cc48dd1abd1
leosar <ldosto>
2021-07-16 17:06:42 +0200

    Initial commit

---
+++ src/main/java/com/leonardo/demos/avaloqassessment/AvaloqAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.demos.avaloqassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaloqAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaloqAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo/demos/avaloqassessment/AvaloqAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo.demos.avaloqassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaloqAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
